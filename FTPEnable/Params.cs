579d511c9e81821284f33e4cb270b6dccb7c9556
Yohoki <Yohoki3@aol.com>
2021-06-18 22:54:12 -0500

    Add project files.

---
+++ FTPEnable/Params.cs
@@ -0,0 +1,78 @@
+﻿namespace FTPEnable
+{
+    /// <summary>
+    /// Class to hold and parse commandline arguments.
+    /// </summary>
+    class Params
+    {
+        private static string User { get; set; }
+        private static string Pass { get; set; }
+        private static string Addr { get; set; }
+        private static string Drive { get; set; }
+
+        private static string Username 
+        { get 
+            { 
+                if (User != "") { return " /user:"; ; }
+                return "";
+            }
+        }
+
+        private static string[] Options 
+        { get 
+            { return new string[] { $"{Drive}: /delete", $"{Drive}: {Addr}/{Pass}{Username}{User} /hide" }; }
+        }
+
+        /// <summary>
+        /// Parses the command line arguments and returns a string[] array.
+        /// </summary>
+        /// <param name="args">Excpects a string array. Commandline Arguments works. Will append Help options if blank.</param>
+        /// <returns></returns>
+        public static string[] ParseArgs(string[] args)
+        {
+            if (args.Length == 0) { args = new string[] { "/h" }; }
+
+            for (var Index = 0; args.Length > Index || args.Length == 0; Index++)
+            {
+                switch (args[Index].ToLower())
+                {
+                    case "/u":
+                    case "/user":
+                    case "-u":
+                    case "-user":
+                        User = $"{args[Index + 1]}";
+                        break;
+                    case "/p":
+                    case "/pass":
+                    case "-p":
+                    case "-pass":
+                        Pass = " " + args[Index + 1];
+                        break;
+                    case "/i":
+                    case "/ip":
+                    case "-i":
+                    case "-ip":
+                        Addr = args[Index + 1];
+                        break;
+                    case "/d":
+                    case "/drive":
+                    case "-d":
+                    case "-drive":
+                        Drive = args[Index + 1];
+                        break;
+                    case "/h":
+                    case "/help":
+                    case "-h":
+                    case "-help":
+                    case "/?":
+                    case "-?":
+                        Program.Help();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return Options;
+        }
+    }
+}
---
+++ FTPEnable/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace FTPEnable
+{
+    class Program
+    {
+        const bool Debug = false;
+        const string FTPUse = @"C:\Program Files\Ferro Software\FTPUse\FtpUse.exe";
+
+        static void Main(string[] args)
+        {
+            if (Debug != true) 
+            {
+                CheckInstall(FTPUse);
+            }
+
+            string[] options = Params.ParseArgs(args);
+            
+            if (Debug == true)
+            {
+                DebugText(options);
+            }
+            
+            if (Debug != true)
+            {
+                Launch(options);
+            }
+
+        }
+
+        public static void Help()
+        {
+            Console.WriteLine("This program is a quick loader for Ferro Software's FTPUse.exe software. It will quickly load the\n" +
+                              "parameters into FTPUse via commandline, and can be run as administrator. To use this program, create\n" +
+                              "a shortcut and add parameters as commandline arguments in the target field.\n\n" +
+                              "Usage:\n\n" +
+                              "\t'/user Username'(Optional)\n" +
+                              "\t\tSpecifies the username.Leave blank for anonymous. (or maybe type aononymous ?)\n" +
+                              "\t'/Pass Password'(Optional)\n" +
+                              "\t\tSpecifies the password.Leave blank if no password is required.\n" +
+                              "\t'/IP IPAddress'(Required)\n" +
+                              "\t\tSpecifies the remote host's IP address.\n" +
+                              "\t'/Drive DriveLetter'(Required)\n" +
+                              "\t\tSpecify a single character for the drive letter.\n" +
+                              "\t'/Help'(Optional)\n" +
+                              "\t\tDisplays this help text.\n\n" +
+                              "Press enter key to exit...");
+            Console.ReadLine();
+            System.Environment.Exit(0);
+        }
+
+        private static void CheckInstall(string FTPUse)
+        {
+            if (!File.Exists(FTPUse))
+            {
+                Console.WriteLine("\n\n\t\tFerro Software's FTPUse.exe not found!\n" +
+                                  "\t\tPlease install FTPUse to Program Files\n\n");
+                Help();
+            }
+        }
+
+        private static void DebugText(string[] options)
+        {
+            Console.WriteLine(options[0] + "\n" + options[1]); Console.ReadLine();
+        }
+
+        private static void Launch(string[] options)
+        {
+            Process StartFTPUse = new Process();
+
+            foreach (string option in options)
+            {
+                StartFTPUse.StartInfo.FileName = FTPUse;
+                StartFTPUse.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                StartFTPUse.StartInfo.UseShellExecute = true;
+                StartFTPUse.StartInfo.Verb = "runas";
+                StartFTPUse.StartInfo.Arguments = option;
+                StartFTPUse.Start();
+            }
+        }
+    }
+}
